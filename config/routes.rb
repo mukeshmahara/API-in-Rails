49454790b03914c37b02589e82214a4b92639bfa
mukeshmahara <mukeshmahara1@gmail.com>
2022-02-06 07:37:51 +0545

    routes changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
-      get 'api/index'
-      resources: login
+  namespace :Api do
+    namespace :V1 do
+      get 'api/index',
+      # resources: logi
     end
   end
 end
